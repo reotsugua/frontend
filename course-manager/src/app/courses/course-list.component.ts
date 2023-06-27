07a62106c7073ebb707b5b86213fa18383b27320
reotsugua <renanaugusto.ras@gmail.com>
2023-06-26 22:28:32 -0300

    add css

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -29,7 +29,7 @@ export class CourseListComponent implements OnInit{
                 price: 45.99,
                 code: 'LKL-1094',
                 duration: 80,
-                rating: 4,
+                rating: 3,
                 relaseDate: 'November, 4, 2019'
             }
         ]
--- course-manager/src/app/star/star.component.ts
+++ course-manager/src/app/star/star.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
-  //styleUrls: ['./star.component.css']
+  styleUrls: ['./star.component.css']
 })
-export class StarComponent {
+export class StarComponent implements OnChanges{
+  @Input()
+  rating: number = 0;
+
+  starWidth: number | undefined;
+  
+  ngOnChanges(): void {
+    this.starWidth = this.rating * 94/5;
+  }
 
 }
