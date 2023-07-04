cbcc5bec7677b67d3be8445f5de5f0167fafae22
reotsugua <renanaugusto.ras@gmail.com>
2023-07-03 21:42:03 -0300

    implementando o delete

--- course-manager/src/app/courses/course.service.ts
+++ course-manager/src/app/courses/course.service.ts
@@ -28,62 +28,66 @@ export class CouseService{
         }
     }
 
+    deleteById(id: number): Observable<any> {
+        return this.httpClient.delete<any>(`${this.courseUrl}/${id}`);
+    }
+
 }
 
-var COURSES: Course[] = [
-    {
-        id: 1,
-        name: 'Angular: CLI',
-        relaseDate: 'November 2, 2019',
-        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-        duration: 120,
-        code: 'XLF-1212',
-        rating: 3,
-        price: 12.99,
-        imageUrl: '/assets/images/cli.png',
-    },
-    {
-        id: 2,
-        name: 'Angular: Forms',
-        relaseDate: 'November 4, 2019',
-        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-        duration: 80,
-        code: 'DWQ-3412',
-        rating: 3.5,
-        price: 24.99,
-        imageUrl: '/assets/images/forms.png',
-    },
-    {
-        id: 3,
-        name: 'Angular: HTTP',
-        relaseDate: 'November 8, 2019',
-        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-        duration: 80,
-        code: 'QPL-0913',
-        rating: 4.0,
-        price: 36.99,
-        imageUrl: '/assets/images/http.png',
-    },
-    {
-        id: 4,
-        name: 'Angular: Router',
-        relaseDate: 'November 16, 2019',
-        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-        duration: 80,
-        code: 'OHP-1095',
-        rating: 4.5,
-        price: 46.99,
-        imageUrl: '/assets/images/router.png',
-    },
-    {
-        id: 5,
-        name: 'Angular: Animations',
-        relaseDate: 'November 25, 2019',
-        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-        duration: 80,
-        code: 'PWY-9381',
-        rating: 5,
-        price: 56.99,
-        imageUrl: '/assets/images/animations.png',
-    }
-];
+// var COURSES: Course[] = [
+//     {
+//         id: 1,
+//         name: 'Angular: CLI',
+//         relaseDate: 'November 2, 2019',
+//         description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+//         duration: 120,
+//         code: 'XLF-1212',
+//         rating: 3,
+//         price: 12.99,
+//         imageUrl: '/assets/images/cli.png',
+//     },
+//     {
+//         id: 2,
+//         name: 'Angular: Forms',
+//         relaseDate: 'November 4, 2019',
+//         description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+//         duration: 80,
+//         code: 'DWQ-3412',
+//         rating: 3.5,
+//         price: 24.99,
+//         imageUrl: '/assets/images/forms.png',
+//     },
+//     {
+//         id: 3,
+//         name: 'Angular: HTTP',
+//         relaseDate: 'November 8, 2019',
+//         description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+//         duration: 80,
+//         code: 'QPL-0913',
+//         rating: 4.0,
+//         price: 36.99,
+//         imageUrl: '/assets/images/http.png',
+//     },
+//     {
+//         id: 4,
+//         name: 'Angular: Router',
+//         relaseDate: 'November 16, 2019',
+//         description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+//         duration: 80,
+//         code: 'OHP-1095',
+//         rating: 4.5,
+//         price: 46.99,
+//         imageUrl: '/assets/images/router.png',
+//     },
+//     {
+//         id: 5,
+//         name: 'Angular: Animations',
+//         relaseDate: 'November 25, 2019',
+//         description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+//         duration: 80,
+//         code: 'PWY-9381',
+//         rating: 5,
+//         price: 56.99,
+//         imageUrl: '/assets/images/animations.png',
+//     }
+// ];
