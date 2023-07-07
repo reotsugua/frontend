f50d7d991b8d47f570bf25260cba7de14188b4e2
reotsugua <renanaugusto.ras@gmail.com>
2023-07-07 19:45:27 -0300

    table com material io

--- cats/src/app/cats/cats.module.ts
+++ cats/src/app/cats/cats.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NewCatComponent } from './new-cat/new-cat.component';
 import { SearchCatsComponent } from './search-cats/search-cats.component';
 import { CatsRoutingModule } from './cats-routing.module';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [NewCatComponent, SearchCatsComponent],
-  imports: [CommonModule, CatsRoutingModule],
+  imports: [CommonModule, CatsRoutingModule, MatTableModule],
   providers: [],
 })
 export class CatsModule {}
