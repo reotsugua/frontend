2c2cbb7101cc9dd88771bf8c109dba40c09474f4
reotsugua <renanaugusto.ras@gmail.com>
2023-07-21 17:01:00 -0300

    desafio

---
+++ js/01VariaveisEOperadores/aula01.js
@@ -0,0 +1,11 @@
+
+let precoCombustivel = 5.99;
+let gastoMedio = 8;
+let distancia = 300;
+
+let litrosKm = distancia/8;
+let gastoRs = litrosKm*precoCombustivel;
+
+//name = "paiva";
+
+console.log(`Foi gasto nessa viagem R$ ${gastoRs}`);
---
+++ js/01VariaveisEOperadores/primeiroDesafio.js
@@ -0,0 +1,12 @@
+
+let precoCombustivel = 5.79;
+let gastoMedio = 10;
+let distancia = 100;
+
+let litrosKm = distancia/gastoMedio;
+let gastoRs = litrosKm*precoCombustivel;
+
+//name = "paiva";
+
+console.log(`Foi gasto nessa viagem R$ ${gastoRs.toFixed(2)}`);
+console.log("oi");
