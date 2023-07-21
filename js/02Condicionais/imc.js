c1f4649f5b1c908c9f67f85a6e8b1056583a6a95
reotsugua <renanaugusto.ras@gmail.com>
2023-07-21 20:21:05 -0300

    desafios

---
+++ js/02Condicionais/imc.js
@@ -0,0 +1,22 @@
+const peso = 65;
+const altura = 1.7;
+const potenciaAltura = Math.pow(altura, 2);
+const imc = peso / potenciaAltura;
+console.log(imc.toFixed(2))
+
+
+if (imc < 18.5) {
+    console.log('abaixo do peso')
+    
+} else if (imc >= 18.5 && imc < 25) {
+    console.log('peso normal')
+    
+} else if (imc >= 25 && imc < 30) {
+    console.log('acima do peso')
+    
+} else if (imc >= 30 && imc < 40) {
+    console.log('obeso')
+    
+} else {
+    console.log('obesidade grave')
+}
