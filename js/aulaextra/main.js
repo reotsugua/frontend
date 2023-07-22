9d92baf262248bf8749b9cdfe18eccfa1d6d687b
reotsugua <renanaugusto.ras@gmail.com>
2023-07-22 15:25:29 -0300

    aula

--- js/aulaextra/main.js
+++ js/aulaextra/main.js
@@ -1,4 +1,5 @@
 let title = document.getElementsByTagName('h1')[0]
+let input = document.getElementsByTagName('input')[0]
 console.log(title);
 
-title.innerHTML = '<h1>oi</h1>'
+title.innerHTML = 'oi'
