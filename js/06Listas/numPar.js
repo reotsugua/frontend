26bace17063f3cc3f2e8c1554f1c80454ee3c2dc
reotsugua <renanaugusto.ras@gmail.com>
2023-07-22 23:42:30 -0300

    for

---
+++ js/06Listas/numPar.js
@@ -0,0 +1,11 @@
+const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < nums.length; i++) {
+    const element = nums[i];
+    
+
+    if (element % 2 == 0) {
+        console.log(element)
+    }
+    
+}
