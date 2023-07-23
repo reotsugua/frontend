4bdfb055b3e33615210d0534d0b6ada0da0c983e
reotsugua <renanaugusto.ras@gmail.com>
2023-07-22 23:57:39 -0300

    debug

---
+++ js/07Importacao/funcoesAuxiliares.js
@@ -0,0 +1,12 @@
+function gets() {
+    return 10;
+}
+
+function print(params) {
+    console.log(params);
+}
+
+module.exports = {
+    gets: gets,
+    print: print
+};
---
+++ js/07Importacao/main.js
@@ -0,0 +1,6 @@
+//const funcoes = require('./funcoesAuxiliares');
+const {gets, print} = require('./funcoesAuxiliares');
+
+//console.log(funcoes.gets())
+
+print(gets());
